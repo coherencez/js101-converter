37814cf9ab43ad2468aa6ac016a1370393a38322
Thomas Swann <tkswann@mac.com>
2016-05-23 23:30:35 -0500

    almost completed excercise, still need 'enter' functionality

--- converter.js
+++ converter.js
@@ -1,26 +1,87 @@
-function toCelsius () {
+// Grab all necessary elements off the page
 
-}
-
-function toFahrenheit () {
-
-}
-
-// Get a reference to the button element in the DOM
-var submitButton = document.getElementById("convert"),
+var convertButton = document.getElementById("convert"),
 		clearButton = document.getElementById("clear"),
 		celsiusRadioSelector = document.getElementById("celsius"),
-    farenheightRadioSelector = document.getElementById("farenheight"),
+    fahrenheitRadioSelector = document.getElementById("fahrenheit"),
     userInput = document.getElementById("userInput"),
-    conversionResult = document.getElementById("jsOutput");
+    messageOutput = document.getElementById("jsMessageOutput");
+    conversionResult = document.getElementById("jsConversionOutput");
 
+// Functions to convert the temp. Takes argument from user input, converts, builds up an HTML 
+// string and sets CSS styles for <section>
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-	
-  console.log("event", clickEvent);
+var convertedF = function toFahrenheit (n) {
+	var num =  Math.floor(((n * (9/5)) + 32)), htmlString = '<p>';
+		htmlString += num;
+		htmlString += '</p>';
+		if (num > 90) {
+			messageOutput.style.color = "rgba(255, 31, 43, 1.0)";
+	} else if (num < 32) {
+		messageOutput.style.color = "rgba(25, 210, 255, 1.0)";
+	} else {
+		messageOutput.style.color= "rgba(0, 221, 1, 1.0)";
+	}
+		messageOutput.style.background = "rgba(255,255,255, .8)";
+		messageOutput.style.border = "5px ridge rgba(255, 255, 255, 1.0)";
+		messageOutput.innerHTML = htmlString;
 }
 
-// Assign a function to be executed when the button is clicked
-// button.addEventListener("click", determineConverter);
+var convertedC = function toCelsius (n) {
+	var num =  Math.floor((n - 32) * (5/9)), htmlString = '<p>';
+		htmlString += num;
+		htmlString += '</p>';
+		if (num > 32) {
+			messageOutput.style.color = "rgba(255, 31, 43, 1.0)";
+	} else if (num < 0) {
+		messageOutput.style.color = "rgba(25, 210, 255, 1.0)";
+	} else {
+		messageOutput.style.color= "rgba(0, 221, 1, 1.0)";
+	}
+		messageOutput.style.background = "rgba(255,255,255, .8)";
+		messageOutput.style.border = "5px ridge rgba(255, 255, 255, 1.0)";
+		messageOutput.innerHTML = htmlString;
+	}
+
+
+
+	// Determine which radio button is selected and run the appropriate function
+
+function determineConverter () {
+	if (fahrenheitRadioSelector.checked) {
+  console.log("fahrenheit selected, converting now...");
+		convertedF(userInput.value);
+	} else if (celsiusRadioSelector.checked){
+		console.log("Celsius selected, converting now...");
+		convertedC(userInput.value);
+	}
+}
+
+// Assign the determineConverter function to be executed when the convert button is clicked
+convertButton.addEventListener('click', determineConverter);
+// convertButton.addEventListener('keydown', function (e) {
+//     var key = e.keyCode;
+//     if (key === 13) { // 13 is enter
+//       // code for enter
+//       determineConverter;
+//     }	
+// });
+
+// clear all fields and start fresh
+
+function clearFields () {
+	fahrenheitRadioSelector.checked = false;
+	celsiusRadioSelector.checked = false;
+	userInput.value = '';
+	messageOutput.innerHTML = '';
+	messageOutput.style.background = '';
+	messageOutput.style.border = '';
+
+	// conversionResult.innerHTML = '';
+	console.log("All fields have been reset!");
+}
+
+// Assign the clear fields function to the clear button
+
+clearButton.addEventListener("click", clearFields);
+
